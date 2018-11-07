03cee76399af26bd129491450e36f829fe8f36ba
JavierLopatin <javierlopatin@gmail.com>
2018-11-07 11:07:48 -0800

    add prebuild funciton

--- 01_LandTrendr_parameterization.js
+++ 01_LandTrendr_parameterization.js
@@ -1,3 +1,4 @@
+
 //########################################################################################################
 //#                                                                                                    #\\
 //#                      LANDTRENDR SOURCE AND FITTING PIXEL TIME SERIES PLOTTING                      #\\
@@ -7,26 +8,32 @@
 // Description: parameterization and visualization of LandTrendr using
 //              different spectral indecies and bandas
 
+// TO use the predefined LandTrendr functions of https://github.com/eMapR/LT-GEE, please open GEE code API
+// at: https://code.earthengine.google.com/?accept_repo=users/emaprlab/public
+// We used a personalized version were we added the EVI index
+
 // date: 2018-11-05
 // author: Javier Lopatin | javier.lopatin@kit.edu; javierlopatin@gmail.com
-// Based on the information posted in: https://github.com/eMapR/LT-GEE
 
 
 //########################################################################################################
 //##### INPUTS #####
 //########################################################################################################
 
+// load the LandTrendr.js module
+//var ltgee = require('users/emaprlab/public:Modules/LandTrendr.js');
+var ltgee = require('users/javierlopatin/SF_delta:LandTrendr.js');
 
-// show LandTrend for this point
+// define parameters
+var startYear = 1985;
+var endYear = 2017;
+var startDay = '06-20';
+var endDay = '09-20';
 var point = ee.FeatureCollection(geometry);
 var aoi = delta;
-
-// load LandTrendr estimation funciton
-var func = require('users/javierlopatin/SF_delta:00_LandTrendr_Functions')
-
-// define the segmentation parameters:
-// reference: Kennedy, R. E., Yang, Z., & Cohen, W. B. (2010). Detecting trends in forest disturbance and recovery using yearly Landsat time series: 1. LandTrendr—Temporal segmentation algorithms. Remote Sensing of Environment, 114(12), 2897-2910.
-var run_params = {
+var index = ['NBR', 'EVI', 'TCB', 'TCG','TCW']
+var ftvList = [];
+var runParams = {
   maxSegments:            6,
   spikeThreshold:         0.9,
   vertexCountOvershoot:   3,
@@ -36,7 +43,7 @@ var run_params = {
   bestModelProportion:    0.75,
   minObservationsNeeded:  6
 };
-
+var maskThese = ['cloud', 'shadow', 'snow', 'water']
 
 //########################################################################################################
 //##### FUNCTIONS FOR EXTRACTING AND PLOTTING A PIXEL TIME SERIES #####
@@ -75,53 +82,19 @@ var chartPoint = function(lt, point, distDir, title) {
   print(ui.Chart(data, 'LineChart', options,{'columns': [0, 1, 2]}));
 };
 
-//########################################################################################################
-//##### RUN LANDTRENDR #####
-//########################################################################################################
-
-// get Landsat 5,7,8 homogenized collection
-var ltCollector = func.ltCollector(run_params, aoi); // (run_param, aoi)
-
-// LT
-// using NDVI
-var lt_ndvi = func.applyLT('NDVI', ltCollector, run_params, -1); //(VI, annualSRcollection, run_params, distDir)
-// using EVI
-var lt_evi = func.applyLT('EVI', ltCollector, run_params, -1);
-// using GNDVI
-var lt_gndvi = func.applyLT('GNDVI', ltCollector, run_params, -1);
-// using LSWI
-var lt_lswi = func.applyLT('LSWI', ltCollector, run_params, -1);
-// using Tasseled-Cup Brightness
-var lt_brightness = func.applyLT('TCB', ltCollector, run_params, 1);
-// using Tasseled-Cup Greenness
-var lt_greenness = func.applyLT('TCG', ltCollector, run_params, 1);
-// using Tasseled-Cup Brightness
-var lt_wetness = func.applyLT('TCW', ltCollector, run_params, 1);
-// using Tasseled-Cup Angle
-var lt_angle = func.applyLT('TCA', ltCollector, run_params, 1);
-// using bands
-var lt_b1 = func.applyLT('B1', ltCollector, run_params, 1);
-var lt_b2 = func.applyLT('B2', ltCollector, run_params, 1);
-var lt_b3 = func.applyLT('B3', ltCollector, run_params, 1);
-var lt_b4 = func.applyLT('B4', ltCollector, run_params, 1);
-var lt_b5 = func.applyLT('B5', ltCollector, run_params, 1);
-var lt_b7 = func.applyLT('B7', ltCollector, run_params, 1);
+// apply LandTrendr.js functions
+var lt0 = ltgee.runLT(startYear, endYear, startDay, endDay, aoi, index[0], ftvList, runParams, maskThese);
+var lt1 = ltgee.runLT(startYear, endYear, startDay, endDay, aoi, index[1], ftvList, runParams, maskThese);
+var lt2 = ltgee.runLT(startYear, endYear, startDay, endDay, aoi, index[2], ftvList, runParams, maskThese);
+var lt3 = ltgee.runLT(startYear, endYear, startDay, endDay, aoi, index[3], ftvList, runParams, maskThese);
+var lt4 = ltgee.runLT(startYear, endYear, startDay, endDay, aoi, index[4], ftvList, runParams, maskThese);
 
 //----- PLOT THE SOURCE AND FITTED TIME SERIES FOR THE GIVEN POINT -----
-chartPoint(lt_ndvi, point, -1, 'NDVI'); // plot the x-y time series for the given point
-chartPoint(lt_evi, point, -1, 'EVI');
-chartPoint(lt_gndvi, point, -1, 'GNDVI');
-chartPoint(lt_lswi, point, -1, 'LSWI');
-chartPoint(lt_brightness, point, 1, 'TCBº');
-chartPoint(lt_greenness, point, 1, 'TCG');
-chartPoint(lt_wetness, point, 1, 'TCW');
-chartPoint(lt_angle, point, 1, 'TCA');
-chartPoint(lt_b1, point, 1, 'B1');
-chartPoint(lt_b2, point, 1, 'B2');
-chartPoint(lt_b3, point, 1, 'B3');
-chartPoint(lt_b4, point, 1, 'B4');
-chartPoint(lt_b5, point, 1, 'B5');
-chartPoint(lt_b7, point, 1, 'B7');
+chartPoint(lt0, point, -1, index[0]); // plot the x-y time series for the given point
+chartPoint(lt1, point, -1, index[1]);
+chartPoint(lt2, point, -1, index[2]);
+chartPoint(lt3, point, -1, index[3]);
+chartPoint(lt4, point, -1, index[4]);
 
 // get errors for each VI
 var getError = function(lt){
@@ -132,20 +105,12 @@ var getError = function(lt){
 }
 
 var error = {
-  'NDVI': getError(lt_ndvi),
-  'EVI': getError(lt_evi),
-  'GNDVI': getError(lt_gndvi),
-  'LSWI': getError(lt_lswi),
-  'TCB': getError(lt_brightness),
-  'TCG': getError(lt_greenness),
-  'TCW': getError(lt_wetness),
-  'TCA': getError(lt_angle),
-  'B1': getError(lt_b1),
-  'B2': getError(lt_b2),
-  'B3': getError(lt_b3),
-  'B4': getError(lt_b4),
-  'B5': getError(lt_b5),
-  'B7': getError(lt_b7),
+  'NBR': getError(lt0),
+  'EVI': getError(lt1),
+  'TCB': getError(lt2),
+  'TCG': getError(lt3),
+  'TCW': getError(lt4),
+
 };
 
 print(error)
